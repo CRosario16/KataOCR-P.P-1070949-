1b5f2bcf1e0f8b265fc4992ca8e27a54a960903d
CRosario16 <40611747+CRosario16@users.noreply.github.com>
2018-06-26 16:36:25 -0400

    Add files via upload
    Esta es la versión 1 de la Kata la cual corresponde al primer parcial.
    
    Complejidad Ciclomatica de la clase: 16
    Cada método de la clase tiene una complejidad menor o igual a 5
    
    BankOCR.cs
    
    ValidateAccount(string) : string        ---       5
    Initialize() : void                                ---       4
    ArrayToNumber(string[]): void         ---       3
    ConvertToNumbers(string[]): void   ---       3
    BANKOCR(string)                             ---       1
    
    BANKOCRTests.cs
    AllZeroesShouldReturnZeroes(): void ---   1
    BANKOCRTests()                              ---      1
    EmptyAccountsShouldReturnAllQuestionMarks(): void  --- 1
    MixedAccounts(): void                    ---      1
    ShouldReturnAllQuestionMarkWithTag() : void --- 1
    ShouldReturnInvalidAccountTag(): void --- 1
    ShouldReturnQuestionMark(): void      ---   1
    ShouldReturnQuestionMarkWithTag(): void --- 1
    ThreeNumbersShouldWorkAsWell(): void ---    1
    TwoNumbersShouldWorkAsWell():void    ---    1
    
    Objetivo: Llevar la complejidad ciclomatica de cada método a menor o igual que 2

---
+++ BANKOCRTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BANKOCR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANKOCR.Tests
+{
+	[TestClass()]
+	public class BANKOCRTests
+	{
+		[TestMethod()]
+		public void AllZeroesShouldReturnZeroes()
+		{
+			List<String> expected = new List<string>() {
+				{"000000000"} 
+			};
+			BANKOCR b = new BANKOCR("D:/Users/franc/Documents/Visual Studio 2017/Projects/BANKOCR/BANKOCR/bin/inputZeroes.txt");
+			CollectionAssert.AreEqual(expected, b.accountsturned);
+		}
+
+		[TestMethod()]
+		public void TwoNumbersShouldWorkAsWell()
+		{
+			List<String> expected = new List<string>() {
+				{ "457508000"},
+				{ "345882865"}
+			};
+			BANKOCR b = new BANKOCR("D:/Users/franc/Documents/Visual Studio 2017/Projects/BANKOCR/BANKOCR/bin/inputTwoNumbers.txt");
+			CollectionAssert.AreEqual(expected, b.accountsturned);
+		}
+		[TestMethod()]
+		public void ThreeNumbersShouldWorkAsWell()
+		{
+			List<String> expected = new List<string>() {
+				{ "457508000"},
+				{ "345882865"},
+				{"457508043" }
+			};
+			BANKOCR b = new BANKOCR("D:/Users/franc/Documents/Visual Studio 2017/Projects/BANKOCR/BANKOCR/bin/inputThreeNumbers.txt");
+			CollectionAssert.AreEqual(expected, b.accountsturned);
+		}
+		[TestMethod()]
+		public void ShouldReturnQuestionMark()
+		{
+			List<String> expected = new List<string>() {
+				{ "345882?65"},
+			};
+			BANKOCR b = new BANKOCR("D:/Users/franc/Documents/Visual Studio 2017/Projects/BANKOCR/BANKOCR/bin/DamagedAccount.txt");
+			CollectionAssert.AreEqual(expected, b.accountsturned);
+		}
+		[TestMethod()]
+		public void ShouldReturnQuestionMarkAndTag()
+		{
+			List<String> expected = new List<string>() {
+				{ "345882?65 ILL"},
+			};
+			BANKOCR b = new BANKOCR("D:/Users/franc/Documents/Visual Studio 2017/Projects/BANKOCR/BANKOCR/bin/DamagedAccount.txt");
+			CollectionAssert.AreEqual(expected, b.accountsturned);
+		}
+		[TestMethod()]
+		public void ShouldReturnAllQuestionMarkWithTag()
+		{
+			List<String> expected = new List<string>() {
+				{ "????????? ILL"},
+			};
+			BANKOCR b = new BANKOCR("D:/Users/franc/Documents/Visual Studio 2017/Projects/BANKOCR/BANKOCR/bin/AllQuestionMark.txt");
+			CollectionAssert.AreEqual(expected, b.accountsturned);
+		}
+		[TestMethod()]
+		public void ShouldReturnInvalidAccountTag()
+		{
+			List<String> expected = new List<string>() {
+				{ "111111111 ERR"},
+			};
+			BANKOCR b = new BANKOCR("D:/Users/franc/Documents/Visual Studio 2017/Projects/BANKOCR/BANKOCR/bin/inputInValid.txt");
+			CollectionAssert.AreEqual(expected, b.accountsturned);
+		}
+		[TestMethod()]
+		public void MixedAccounts()
+		{
+			List<String> expected = new List<string>() {
+				{ "111111111 ERR"},
+				{ "457508000"},
+				{ "345882?65 ILL"}
+			};
+			BANKOCR b = new BANKOCR("D:/Users/franc/Documents/Visual Studio 2017/Projects/BANKOCR/BANKOCR/bin/inputInvalidAndValid.txt");
+			CollectionAssert.AreEqual(expected, b.accountsturned);
+		}
+		[TestMethod()]
+		public void EmptyAccountsShouldReturnAllQuestionMarks()
+		{
+			List<String> expected = new List<string>() {
+				{ "????????? ILL"},
+			};
+			BANKOCR b = new BANKOCR("D:/Users/franc/Documents/Visual Studio 2017/Projects/BANKOCR/BANKOCR/bin/EmptyAccount.txt");
+			CollectionAssert.AreEqual(expected, b.accountsturned);
+		}
+
+	}
+}
---
+++ BankOCR.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANKOCR
+{
+	public class BANKOCR{
+		string[] file;
+		List<List<string>> accounts;
+		public List<String> accountsturned;
+		Dictionary<string, char> numbers = new Dictionary<string, char>()
+		{
+			{ " _ "+ "| |"+ "|_|", '0'},
+			{ "   "+ " | "+ " | ", '1'},
+			{ " _ "+ " _|"+ "|_ ", '2'},
+			{ " _ "+ " _|"+ " _|", '3'},
+			{ "   "+ "|_|"+ "  |", '4'},
+			{ " _ "+ "|_ "+ " _|", '5'},
+			{ " _ "+ "|_ "+ "|_|", '6'},
+			{ " _ "+ "  |"+ "  |", '7'},
+			{ " _ "+ "|_|"+ "|_|", '8'},
+			{ " _ "+ "|_|"+ " _|", '9'}
+		};
+
+		public BANKOCR(string path)
+		{
+		file =	System.IO.File.ReadAllLines(path);
+			Initialize();
+		}
+		public void Initialize()
+		{
+			accounts = new List<List<string>>();
+			accountsturned = new List<string>();
+			int y = -1;
+			for (int i = 0; i < file.Length; i+=4)
+			{
+
+				y++;
+				accounts.Add(new List<string>());
+				for (int j = 0; j < 3; j++)
+				{
+					accounts[y].Add(file[i + j]);
+				}
+			}
+			for(int i = 0; i < accounts.Count; i++)
+			{
+				ConvertToNumbers(accounts[i].ToArray());
+			}
+		}
+		public void ConvertToNumbers(string[] account)
+		{
+			string[] toconvert = new string[9];
+			
+			int addee = 0;
+			for (int j = 0; j < 9; j++)
+			{
+				if (j != 0) addee = (j * 3); 
+				toconvert[j] += account[0].Substring(addee, 3);
+				toconvert[j] += account[1].Substring(addee, 3);
+				toconvert[j] += account[2].Substring(addee, 3);
+			}
+			ArrayToNumber(toconvert);
+		}
+
+		public void ArrayToNumber(string[] account)
+		{
+			string finalNumber = "";
+			
+			for (int i = 0; i < account.Length; i++)
+			{
+				finalNumber += numbers.ContainsKey(account[i]) ? numbers[account[i]] : '?';
+			}
+			finalNumber = ValidateAccount(finalNumber);
+			accountsturned.Add(finalNumber);
+		}
+
+		public string ValidateAccount(string number)
+		{
+			string n = number;
+			int output = 0;
+			int calc = 0;
+			if(int.TryParse(n,out output))
+			{
+				for (int i = 0; i < 9; i++)
+				{
+					calc += Convert.ToInt32(n[i] + "")*(n.Length-i);
+				}
+				if(calc % 11 != 0)
+				{
+					n += " ERR";
+				}
+			}
+			else
+			{
+				n += " ILL";
+			}
+			return n;
+		}
+		
+
+}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BANKOCR FirstTry = new BANKOCR("D:/Users/franc/Documents/Visual Studio 2017/Projects/BANKOCR/BANKOCR/bin/DamagedAccount.txt");
+			Console.ReadLine();
+		}
+	}
+}
